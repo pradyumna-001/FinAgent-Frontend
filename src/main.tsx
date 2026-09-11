2658ac500b6980ca4861ac52c5bc959a12a61e38
prady001 <gabriel.prady1@gmail.com>
2026-09-11 15:24:42 -0300

    feat(providers): add QueryProvider with retry config

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryProvider } from './providers/QueryProvider';
 import { RouterProvider } from 'react-router-dom';
 import { AuthProvider } from './providers/AuthProvider';
 import { ThemeProvider } from './providers/ThemeProvider';
@@ -8,17 +8,15 @@ import { Toaster } from './components/ui/toaster/Toaster';
 import { router } from './router';
 import './index.css';
 
-const queryClient = new QueryClient();
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <QueryProvider>
       <AuthProvider>
         <ThemeProvider>
           <RouterProvider router={router} />
           <Toaster />
         </ThemeProvider>
       </AuthProvider>
-    </QueryClientProvider>
+    </QueryProvider>
   </StrictMode>,
 );

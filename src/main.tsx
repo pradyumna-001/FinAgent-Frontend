741b72bb8d11699ea871af7f6881164fd9ee3b72
prady001 <gabriel.prady1@gmail.com>
2026-09-11 10:15:28 -0300

    feat(app): integrate providers and add Toaster

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,24 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RouterProvider } from 'react-router-dom';
 import { AuthProvider } from './providers/AuthProvider';
-import { ThemeProvider } from './providers/ThemeProvider.tsx';
+import { ThemeProvider } from './providers/ThemeProvider';
+import { Toaster } from './components/ui/toaster/Toaster';
+import { router } from './router';
 import './index.css';
-import App from './App.tsx';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <ThemeProvider>
+          <RouterProvider router={router} />
+          <Toaster />
+        </ThemeProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
